f7b7c94746cfd473e41d3e744caf42c79b72caa2
Ubuntu <835943102@qq.com>
2017-11-27 21:26:05 +0800

    ok

--- src/TableE.java
+++ src/TableE.java
@@ -15,20 +15,11 @@ public class TableE {
             return true;
         return false;
     }
-    public boolean cmpTnT(String nT,char yT){//查看表格元素是否已经创建。
-        if(this.yT==yT&&this.nT.equals(nT))
-            return true;
-        else
-            return false;
-    }
+
     public void print(){//打印当前的表格对象
         System.out.println("["+this.nT+"] ["+this.yT+"] ("+this.exp+ ") ");
     }
-    public String getExp(){//获取表格对象的内容,文法表达式。
-        return this.exp;
-    }
     public String getNT() {//获得非终结符。
-
         return this.nT;
     }
 }
